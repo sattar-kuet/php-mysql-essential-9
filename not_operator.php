eaacb6e8fe597b1ba98dd8f3fd51090281d603e6
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 11:35:49 +0600

    first commit

---
+++ not_operator.php
@@ -0,0 +1,25 @@
+<?php
+// $a = 20;
+
+// if(!($a==20)){
+//   echo 'here';
+// }
+
+// $b = 10;
+
+// if(!($b>5)){
+// 	echo 'inside if block';
+
+// }
+
+// $active = false; //false 1==1
+
+// if(!$active){
+// 	echo'inactive';
+// }
+
+$a = 10; // $a=true;
+if(!$a){
+  echo 'ZERO VALUE';
+}
+?>
